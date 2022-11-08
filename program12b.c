bf2a52ff0a45a9896714047d85bd417f41e61bd0
Shivang-Mandal <110190685+Shivang-Mandal@users.noreply.github.com>
2022-11-08 23:34:21 +0530

    Add files via upload

---
+++ program12b.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+    int num, n, factorial;
+    printf("Enter a number:- ");
+    scanf("%d",&num);
+
+     if (n == 0)
+        return 1;
+    return n * (n - 1);
+
+
+    printf("Factorial of %d is %d", num, n );
+    return 0;
+}
